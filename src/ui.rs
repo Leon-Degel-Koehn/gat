23084ae2be8f28f7dac83a302f0da8468b0b7b8a
Leon-Degel-Koehn <leon.koehn2002@gmail.com>
2024-10-19 16:40:35 +0200

    Refactor render editing popup and currently editing text

--- src/ui.rs
+++ src/ui.rs
@@ -50,40 +50,7 @@ pub fn ui(frame: &mut Frame, app: &App) {
     frame.render_stateful_widget(list, main_chunks[0], &mut list_state);
     frame.render_widget(profile_content, main_chunks[1]);
 
-    let current_navigation_text = vec![
-        // The first half of the text
-        match app.current_screen {
-            CurrentScreen::Main => Span::styled("Normal Mode", Style::default().fg(Color::Green)),
-            CurrentScreen::Editing => {
-                Span::styled("Editing Mode", Style::default().fg(Color::Yellow))
-            }
-            _ => Span::styled("Normal Mode", Style::default().fg(Color::Green)),
-        }
-        .to_owned(),
-        Span::styled(" | ", Style::default().fg(Color::White)),
-        {
-            if let Some(editing) = &app.currently_editing {
-                match editing {
-                    CurrentlyEditing::Username => {
-                        Span::styled("Editing username", Style::default().fg(Color::Green))
-                    }
-                    CurrentlyEditing::Email => {
-                        Span::styled("Editing email", Style::default().fg(Color::Green))
-                    }
-                    CurrentlyEditing::Alias => {
-                        Span::styled("Editing alias", Style::default().fg(Color::Green))
-                    }
-                    CurrentlyEditing::Token => {
-                        Span::styled("Editing token", Style::default().fg(Color::Green))
-                    }
-                }
-            } else {
-                Span::styled("Not Editing Anything", Style::default().fg(Color::DarkGray))
-            }
-        },
-    ];
-
-    let mode_footer = Paragraph::new(Line::from(current_navigation_text))
+    let mode_footer = Paragraph::new(Line::from(current_navigation_text(&app)))
         .block(Block::default().borders(Borders::ALL));
 
     let key_notes_footer = Paragraph::new(Line::from(key_hints(&app.current_screen)))
@@ -97,60 +64,64 @@ pub fn ui(frame: &mut Frame, app: &App) {
     frame.render_widget(mode_footer, footer_chunks[0]);
     frame.render_widget(key_notes_footer, footer_chunks[1]);
 
-    if let Some(editing) = &app.currently_editing {
-        let popup_block = Block::default()
-            .title("Edit Git Profile")
-            .borders(Borders::ALL)
-            .border_type(BorderType::Rounded)
-            .border_style(Style::default().fg(Color::White))
-            .style(Style::default().bg(Color::DarkGray));
-
-        let area = fixed_size_centered_rect(60, 14, frame.area());
-        frame.render_widget(popup_block, area);
-
-        let popup_chunks = Layout::default()
-            .direction(Direction::Vertical)
-            .margin(1)
-            .constraints([
-                Constraint::Percentage(25),
-                Constraint::Percentage(25),
-                Constraint::Percentage(25),
-                Constraint::Percentage(25),
-            ])
-            .split(area);
-
-        let mut alias_block = Block::default()
-            .title("Profile Alias (not visible in git)")
-            .borders(Borders::ALL);
-        let mut username_block = Block::default().title("Username").borders(Borders::ALL);
-        let mut email_block = Block::default().title("Email").borders(Borders::ALL);
-        let mut token_block = Block::default().title("PA-Token").borders(Borders::ALL);
-
-        let active_style = Style::default().bg(Color::LightYellow).fg(Color::Black);
-
-        match editing {
-            CurrentlyEditing::Alias => alias_block = alias_block.style(active_style),
-            CurrentlyEditing::Username => username_block = username_block.style(active_style),
-            CurrentlyEditing::Email => email_block = email_block.style(active_style),
-            CurrentlyEditing::Token => token_block = token_block.style(active_style),
-        };
-
-        let alias_text = Paragraph::new(app.alias_input.clone()).block(alias_block);
-        let username_text = Paragraph::new(app.username_input.clone()).block(username_block);
-        let email_text = Paragraph::new(app.email_input.clone()).block(email_block);
-        let token_text = Paragraph::new(app.token_input.clone()).block(token_block);
-        frame.render_widget(alias_text, popup_chunks[0]);
-        frame.render_widget(username_text, popup_chunks[1]);
-        frame.render_widget(email_text, popup_chunks[2]);
-        frame.render_widget(token_text, popup_chunks[3]);
-    }
-
     let _ = match app.current_screen {
         CurrentScreen::Cloning => render_cloning_popup(frame, app.clone_url_input.clone()),
         CurrentScreen::Deleting => render_deleting_popup(frame),
+        CurrentScreen::Editing => render_editing_popup(frame, &app),
         CurrentScreen::Injecting => render_injecting_popup(frame),
         _ => {}
     };
+}
+
+fn render_editing_popup(frame: &mut Frame, app: &App) {
+    let Some(editing) = &app.currently_editing else {
+        return;
+    };
+    let popup_block = Block::default()
+        .title("Edit Git Profile")
+        .borders(Borders::ALL)
+        .border_type(BorderType::Rounded)
+        .border_style(Style::default().fg(Color::White))
+        .style(Style::default().bg(Color::DarkGray));
+
+    let area = fixed_size_centered_rect(60, 14, frame.area());
+    frame.render_widget(popup_block, area);
+
+    let popup_chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(1)
+        .constraints([
+            Constraint::Percentage(25),
+            Constraint::Percentage(25),
+            Constraint::Percentage(25),
+            Constraint::Percentage(25),
+        ])
+        .split(area);
+
+    let mut alias_block = Block::default()
+        .title("Profile Alias (not visible in git)")
+        .borders(Borders::ALL);
+    let mut username_block = Block::default().title("Username").borders(Borders::ALL);
+    let mut email_block = Block::default().title("Email").borders(Borders::ALL);
+    let mut token_block = Block::default().title("PA-Token").borders(Borders::ALL);
+
+    let active_style = Style::default().bg(Color::LightYellow).fg(Color::Black);
+
+    match editing {
+        CurrentlyEditing::Alias => alias_block = alias_block.style(active_style),
+        CurrentlyEditing::Username => username_block = username_block.style(active_style),
+        CurrentlyEditing::Email => email_block = email_block.style(active_style),
+        CurrentlyEditing::Token => token_block = token_block.style(active_style),
+    };
+
+    let alias_text = Paragraph::new(app.alias_input.clone()).block(alias_block);
+    let username_text = Paragraph::new(app.username_input.clone()).block(username_block);
+    let email_text = Paragraph::new(app.email_input.clone()).block(email_block);
+    let token_text = Paragraph::new(app.token_input.clone()).block(token_block);
+    frame.render_widget(alias_text, popup_chunks[0]);
+    frame.render_widget(username_text, popup_chunks[1]);
+    frame.render_widget(email_text, popup_chunks[2]);
+    frame.render_widget(token_text, popup_chunks[3]);
 }
 
 fn key_hints<'a>(current_screen: &CurrentScreen) -> Span<'a> {
@@ -174,6 +145,42 @@ fn key_hints<'a>(current_screen: &CurrentScreen) -> Span<'a> {
             Span::styled("(y) confirm/ (n) abort", Style::default().fg(Color::Red))
         }
     }
+}
+
+fn current_navigation_text<'a>(app: &App) -> Vec<Span<'a>> {
+    let mut menu_items = Vec::new();
+    menu_items.push(
+        match &app.current_screen {
+            CurrentScreen::Main => Span::styled("Normal Mode", Style::default().fg(Color::Green)),
+            CurrentScreen::Editing => {
+                Span::styled("Editing Mode", Style::default().fg(Color::Yellow))
+            }
+            _ => Span::styled("Normal Mode", Style::default().fg(Color::Green)),
+        }
+        .to_owned(),
+    );
+    menu_items.push(Span::styled(" | ", Style::default().fg(Color::White)));
+    menu_items.push({
+        if let Some(editing) = &app.currently_editing {
+            match editing {
+                CurrentlyEditing::Username => {
+                    Span::styled("Editing username", Style::default().fg(Color::Green))
+                }
+                CurrentlyEditing::Email => {
+                    Span::styled("Editing email", Style::default().fg(Color::Green))
+                }
+                CurrentlyEditing::Alias => {
+                    Span::styled("Editing alias", Style::default().fg(Color::Green))
+                }
+                CurrentlyEditing::Token => {
+                    Span::styled("Editing token", Style::default().fg(Color::Green))
+                }
+            }
+        } else {
+            Span::styled("Not Editing Anything", Style::default().fg(Color::DarkGray))
+        }
+    });
+    menu_items
 }
 
 fn render_cloning_popup(frame: &mut Frame, clone_url: String) {
