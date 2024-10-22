3aa0a6be3d0d78fa760009d370df56a0d72da75d
Leon-Degel-Koehn <leon.koehn2002@gmail.com>
2024-10-22 13:09:25 +0200

    Make Deleting popup fixed size

--- src/ui.rs
+++ src/ui.rs
@@ -226,7 +226,7 @@ fn render_deleting_popup(frame: &mut Frame) {
         .block(popup_block)
         .wrap(Wrap { trim: false });
 
-    let area = centered_rect(60, 25, frame.area());
+    let area = fixed_size_centered_rect(60, 3, frame.area());
     frame.render_widget(exit_paragraph, area);
 }
 
@@ -268,29 +268,6 @@ fn fixed_size_centered_rect(width: u16, height: u16, r: Rect) -> Rect {
             Constraint::Length(r.width / 2 - width / 2),
             Constraint::Length(width),
             Constraint::Length(r.width / 2 - width / 2),
-        ])
-        .split(popup_layout[1])[1] // Return the middle chunk
-}
-
-/// helper function to create a centered rect using up certain percentage of the available rect `r`
-fn centered_rect(percent_x: u16, percent_y: u16, r: Rect) -> Rect {
-    // Cut the given rectangle into three vertical pieces
-    let popup_layout = Layout::default()
-        .direction(Direction::Vertical)
-        .constraints([
-            Constraint::Percentage((100 - percent_y) / 2),
-            Constraint::Percentage(percent_y),
-            Constraint::Percentage((100 - percent_y) / 2),
-        ])
-        .split(r);
-
-    // Then cut the middle vertical piece into three width-wise pieces
-    Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([
-            Constraint::Percentage((100 - percent_x) / 2),
-            Constraint::Percentage(percent_x),
-            Constraint::Percentage((100 - percent_x) / 2),
         ])
         .split(popup_layout[1])[1] // Return the middle chunk
 }
